d05b3dc73eda07970e5a14472b600fb0ebb816dc
Lucas Ribeiro <lucasribeirodesouza2000@gmail.com>
2019-07-12 00:26:53 -0300

    Terminando o site ASV ...

---
+++ MudaFoto/muda.js
@@ -0,0 +1,18 @@
+function inicio(){
+    document.getElementById("troca").src = "../MudaFoto/icones/casa.png";
+}
+function sem_nada(){
+    document.getElementById("troca").src = "../MudaFoto/icones/logo2.jpg";
+}
+function quem_somos(){
+    document.getElementById("troca").src = "../MudaFoto/icones/quem-somos.png";
+}
+function onde_estamos(){
+    document.getElementById("troca").src = "../MudaFoto/icones/onde-estamos.png";
+}
+function seguros(){
+    document.getElementById("troca").src = "../MudaFoto/icones/seguros.jpg";
+}
+function contato(){
+    document.getElementById("troca").src = "../MudaFoto/icones/contato.png";
+}
